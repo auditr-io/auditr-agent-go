0160cbe69e2aae3e45698eeccb6eb717f1130ba0
cyyapye <ye@alcarus.com>
2021-04-09 22:16:55 -0700

    fix: repeatable httpclient test

--- config/httpclient_test.go
+++ config/httpclient_test.go
@@ -10,22 +10,23 @@ import (
 
 func TestNewHTTPClient_ReusesTransport(t *testing.T) {
 	var wg sync.WaitGroup
-	var clients []*http.Client
 	expectedClients := 10
 	wg.Add(expectedClients)
 
+	clients := make([]*http.Client, expectedClients)
 	for i := 0; i < expectedClients; i++ {
-		go func() {
+		go func(n int) {
 			defer wg.Done()
 			client, err := NewHTTPClient("https://auditr.io")
 			assert.NoError(t, err)
-			clients = append(clients, client)
-		}()
+			clients[n] = client
+		}(i)
 	}
 
 	wg.Wait()
 	assert.Equal(t, expectedClients, len(clients))
-	for _, c := range clients {
+	for i, c := range clients {
+		assert.NotNil(t, clients[i])
 		assert.Equal(t, clients[0].Transport, c.Transport)
 	}
 }
