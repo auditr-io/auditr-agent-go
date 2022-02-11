78133b8ac00d6ba22db2262fd38ba96a8460716e
cyyapye <ye@alcarus.com>
2022-02-10 21:54:18 -0800

    fix: fix copywriter test

--- wrappers/common/copywriter_test.go
+++ wrappers/common/copywriter_test.go
@@ -3,29 +3,30 @@ package common
 import (
 	"io/ioutil"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCopyWriter(t *testing.T) {
+	expectedStatusCode := 200
+	expectedHeaders := http.Header{
+		"Content-Type": {"application/json"},
+	}
+	expectedBodyBytes := []byte(`{
+		"hi": "you"
+	}`)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", func(w http.ResponseWriter, _ *http.Request) {
 		cw := NewCopyWriter(w)
 
-		expectedStatusCode := 200
-		expectedHeaders := http.Header{
-			"Content-Type": {"application/json"},
-		}
-		expectedBodyBytes := []byte(`{
-			"hi": "you"
-		}`)
-
 		for k, v := range expectedHeaders {
-			w.Header().Add(k, v[0])
+			cw.Header().Add(k, v[0])
 		}
-		w.WriteHeader(expectedStatusCode)
-		w.Write(expectedBodyBytes)
+		cw.WriteHeader(expectedStatusCode)
+		cw.Write(expectedBodyBytes)
 
 		res := cw.Response()
 
@@ -35,4 +36,15 @@ func TestCopyWriter(t *testing.T) {
 		body, _ := ioutil.ReadAll(res.Body)
 		assert.Equal(t, expectedBodyBytes, body)
 	})
+
+	w := httptest.NewRecorder()
+	r, _ := http.NewRequest("POST", "/", nil)
+	mux.ServeHTTP(w, r)
+
+	res := w.Result()
+	assert.Equal(t, expectedStatusCode, res.StatusCode)
+	assert.Equal(t, expectedHeaders, res.Header)
+
+	body, _ := ioutil.ReadAll(res.Body)
+	assert.Equal(t, expectedBodyBytes, body)
 }
